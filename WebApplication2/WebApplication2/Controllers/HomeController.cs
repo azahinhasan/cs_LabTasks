f10108dda2f5c612860f328c2c28b6bf3ea79ea1
A H Zahin <azahinhasan@gmail.com>
2021-02-01 14:06:05 +0600

    'T_2'

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(int? id) //nullable type 'int?'
+        {
+            return "hellow"+id;
+        }
+
+
+    }
+}
