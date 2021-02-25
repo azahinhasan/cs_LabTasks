56ef220bc8300f137dcbcf5eef81715ad6560279
A H Zahin <azahinhasan@gmail.com>
2021-02-25 12:24:39 +0600

    With Entity Framework scaffolding technique

---
+++ WebApplication5 - Copy/WebApplication5/Controllers/CategoryController.cs
@@ -0,0 +1,77 @@
+﻿using WebApplication5.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+using System.Data.Entity;
+
+namespace WebApplication5.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+
+        Models.InventoryDbEntities context = new Models.InventoryDbEntities();
+
+
+
+        public ActionResult Index()
+        {
+            return View(context.Catagories.ToList());
+        }
+
+        [HttpGet]
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Catagory category )
+        {
+            context.Catagories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit( int id)
+        {
+            var categoryToEdit = context.Catagories.Find(id); //find only for Primery key
+            //var categoryToEdit = context.Catagories.Where(x => x.CateforyName=="cloth").FirstOrdDefult(); //for others
+            
+            return View(categoryToEdit);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id,Catagory category)
+        {
+            category.CatagoriesId = id;
+            var categoryToEdit = context.Catagories.Find(id);
+            
+            categoryToEdit.CatagoriesName = category.CatagoriesName;
+            //context.Entry(category).State = EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]   
+        public ActionResult Delete(int id)
+        {
+            var categoryToEdit = context.Catagories.Find(id);
+            return View(categoryToEdit);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+
+            context.Catagories.Remove(context.Catagories.Find(id));
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication5 - Copy/WebApplication5/Controllers/ProductController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        Models.InventoryDbEntities context = new Models.InventoryDbEntities();
+        public ActionResult Index()
+        {
+            return View(context.Products.ToList());
+            //return View(context.Products.Find()); //for 1 or selected products
+        }
+
+        [HttpGet]
+
+        public ActionResult Create()
+        {
+
+            ViewData["catagories"] = context.Catagories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Create(Product product)
+        {
+            if (product.ProductsName==null)
+            {
+                ViewData["error"] = "Product Name not Given";
+                ViewData["catagories"] = context.Catagories.ToList();
+                return View();
+            }
+            else
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+
+        [HttpGet]
+
+        public ActionResult Edit(int id)
+        {
+            ViewData["catagories"] = context.Catagories.ToList();
+            return View(context.Products.Find(id));
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Edit(int id,Product product)
+        {
+            product.ProductsId = id;
+            context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public ActionResult Delete(int id)
+        {
+            return View(context.Products.Find(id));
+        }
+
+
+        [HttpPost,ActionName("Delete")]
+
+        public ActionResult ConfirmDelete(int id, Product product)
+        {
+            context.Products.Remove(context.Products.Find(id));
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public ActionResult TopTwoProducts()
+        {
+            var list = context.Products.OrderByDescending(x => x.Price).Take(2).ToList();
+            // ^Its called Lambda Expression
+
+            /*var list= from item in context.Products
+                      orderby item.Price descending
+                      select item;
+            return View(list.Take(2));*/
+            return View(list.Take(2));
+        }
+
+    }
+}
